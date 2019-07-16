432c55bf88bcedb25d70fc6586a8b6658b7de538
Andrew Marino <andrew@andrewmarino.net>
2019-07-15 23:01:31 -0400

    install kirby-meta-tags

--- site/templates/partials/head.blade.php
+++ site/templates/partials/head.blade.php
@@ -2,8 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>{{ $site->title()->html() }} | {{ $page->title()->html() }}</title>
-  <meta name="description" content="{{ $site->description()->html() }}">
+  {!! $page->metaTags() !!}
   <link rel="stylesheet" href="{{ mix('styles/main.css') }}">
   <link rel="icon" href="/assets/dist/images/favicon.png" type="image/x-icon">
 </head>
