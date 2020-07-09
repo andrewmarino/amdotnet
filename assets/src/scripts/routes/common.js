ac93f54e8df5951909b2477d7205bda3f12e1bad
Andy Marino <andy@unitywebagency.com>
2020-07-08 20:25:23 -0400

    remove fontfaceobserver

--- assets/src/scripts/routes/common.js
+++ assets/src/scripts/routes/common.js
@@ -1,42 +1,14 @@
-import FontFaceObserver from 'fontfaceobserver';
 import LazyLoad from 'vanilla-lazyload';
 
 export default {
   init() {
     document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js');
-
-    let lazyLoadInstance = new LazyLoad({
-      elements_selector: '.lazy',
-    });
   },
   finalize() {
-    let fontFamilies = {
-      'Chivo': [
-        {
-          weight: 400,
-        },
-        {
-          weight: 400,
-          style: 'italic',
-        },
-        {
-          weight: 700
-        },
-      ],
-    }
-    let fontObservers = [];
-
-    Object.keys(fontFamilies).forEach((family) => {
-      fontObservers.push(fontFamilies[family].map((config) => {
-        return new FontFaceObserver(family, config);
-      }));
+    let lazyLoad = new LazyLoad({
+      elements_selector: '.lazy',
+      threshold: 10,
+      callback_loaded: el => el.parentNode.classList.remove('loading'),
     });
-
-    Promise.all(fontObservers)
-      .then(function() {
-        document.body.classList.add('fonts-loaded');
-      }, function() {
-        console.log('Fonts not available.');
-      });
   },
 };
