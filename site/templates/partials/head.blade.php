213ec12e11f37e3752b990a651a6a5e5da9e95f7
Andy Marino <andy@unitywebagency.com>
2020-11-16 22:52:59 -0500

    preconnect and async google fonts css
    https://csswizardry.com/2020/05/the-fastest-google-fonts

--- site/templates/partials/head.blade.php
+++ site/templates/partials/head.blade.php
@@ -3,7 +3,9 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   {!! $page->metaTags() !!}
-  <link href="https://fonts.googleapis.com/css?family=Chivo:400,400i,700&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Chivo:400,400i,700&display=swap" as="style" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Chivo:400,400i,700&display=swap" media="print" onload="this.media='all'" />
   <link rel="stylesheet" href="{{ mix('styles/main.css') }}">
   <link rel="icon" href="/assets/dist/images/favicon.png" type="image/x-icon">
 </head>
