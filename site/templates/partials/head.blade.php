33c632ce7112ca58edc9c8b34673984aea79b855
Andrew Marino <andrew@andrewmarino.net>
2021-07-04 22:52:33 -0400

    add meta tags to head, update deps

--- site/templates/partials/head.blade.php
+++ site/templates/partials/head.blade.php
@@ -1,7 +1,7 @@
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  {!! $page->metaTags() !!}
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link rel="preload" href="https://fonts.googleapis.com/css?family=Chivo:400,400i,700&display=swap" as="style" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Chivo:400,400i,700&display=swap" media="print" onload="this.media='all'" />
